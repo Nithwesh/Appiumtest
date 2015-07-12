85acd0ceb13e8082c54c3efad9d1b1b137a70e43
nithwesh <nithwesh@gmail.com>
2015-07-12 00:36:13 -0700

    first commit

---
+++ src/test/java/com/keyhorizons/appium/TestLaunchApp/TestLaunch.java
@@ -0,0 +1,18 @@
+package com.keyhorizons.appium.TestLaunchApp;
+
+import io.appium.java_client.android.AndroidDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class TestLaunch {
+	
+	@Test
+	public void testLaunch() {
+		
+	}
+
+}
